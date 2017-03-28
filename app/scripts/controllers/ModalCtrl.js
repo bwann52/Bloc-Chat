83fcec0ca6306e11c7bd39035e5c08f29140f92f
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-03-27 22:46:41 -0700

    fixes modal

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,6 +1,23 @@
 (function() {
     function ModalCtrl(Room,$uibModal) {
          
+          var template = "<div class='modal-dialog' role='document'>" +
+          "<div class='content'>" +
+            "<div class='modal-header'>" +
+                "<h4>Create a new room</h4>" +
+            "</div>" +
+            "<div class='modal-body'>" +
+              "<p>Enter a room name</p>" +
+              "<div class='room-name'>" +
+                "<input type='textbox' placeholder='enter a room name here' ng-model='$ctrl.roomName'></div>" +
+              "</div>" +
+            "</div>" +
+            "<div class='modal-footer'>" +
+              "<button class='btn btn-success' ng-click='$ctrl.save() && $ctrl.cancel()'>Okay</button>" +
+              "<button class='btn' ng-click='$ctrl.cancel()'>Cancel</button>" +
+            "</div>" +
+          "</div>" +
+        "</div>"
 				
          
         this.open = function(){
@@ -8,7 +25,7 @@
           var modalInstance = $uibModal.open({
               ariaLabelledBy: 'modal-title',
               ariaDescribedBy: 'modal-body',
-              templateUrl: 'modal.html',
+              template: template,
               controller: 'ModalInstanceCtrl',
               controllerAs: '$ctrl',
 					});
